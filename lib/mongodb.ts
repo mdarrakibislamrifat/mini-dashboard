fc30842eb2ef1969ef914a6810b4c3d78bd2024e
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-23 02:06:39 +0600

    Mongodb update

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -25,7 +25,9 @@ async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose
+      .connect(MONGODB_URI!)
+      .then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
